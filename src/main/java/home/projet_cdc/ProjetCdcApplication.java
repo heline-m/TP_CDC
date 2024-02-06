394fd9dfe1ae4de21d6bfdebc1182bdca77a3336
heline-m <eline.malherbe@epsi.fr>
2024-02-06 23:24:59 +0100

    init project

---
+++ src/main/java/home/projet_cdc/ProjetCdcApplication.java
@@ -0,0 +1,13 @@
+package home.projet_cdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetCdcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetCdcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/home/projet_cdc/ProjetCdcApplicationTests.java
@@ -0,0 +1,15 @@
+package home.projet_cdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetCdcApplicationTests {
+
+    @Test
+    void Test1() {
+
+    }
+
+
+}
